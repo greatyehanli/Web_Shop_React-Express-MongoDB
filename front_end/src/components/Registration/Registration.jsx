ce5c73771f6586f065bc24295e00923634cdb729
李耶罕 <liyehan@MacBook-Pro-2.local>
2021-03-21 23:54:43 +0800

    solved bugs

--- front_end/src/components/Registration/Registration.jsx
+++ front_end/src/components/Registration/Registration.jsx
@@ -57,12 +57,12 @@ class Registration extends Component {
         try {
             if(!isSeller){
                 // console.log('Not selle: ', username, password, email);
-
+                console.log("Post Data: ", {username, password, email}, config);
                 var {data} = await axios.post('/toBackendServer/to/auth/register', {username, password, email}, config)
             }else{
                 // console.log('Not User: ', username, password, email);
 
-
+                console.log("Post Data: ", {name: username, password, email}, config);
                 var {data} = await axios.post('/toBackendServer/seller', {name: username, password, email}, config)
             }
 
